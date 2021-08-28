6a44a4583d4eff713f3f43765d3efc72b5b269da
vivek <vivek.iitd93@gmail.com>
2021-08-28 14:53:27 +0530

    Show: error in validation by user itself

--- employeeAppjs/js/validate.js
+++ employeeAppjs/js/validate.js
@@ -1,15 +1,24 @@
-const salary = document.querySelector("#salary");
-const output = document.querySelector(".salary-output");
-output.textContent = salary.value;
-salary.addEventListener("input", function () {
+window.addEventListener("DOMContentLoaded", (event) => {
+  const name = document.querySelector("#name");
+  const textError = document.querySelector(".text-error");
+  name.addEventListener("input", function () {
+    if (name.value.length == 0) {
+      textError.textContent = "";
+      return;
+    }
+    try {
+      new PayrollModel().name = name.value;
+      textError.textContent = "";
+    } catch (e) {
+      textError.textContent = e;
+    }
+  });
+  const salary = document.querySelector("#salary");
+  const output = document.querySelector(".salary-output");
   output.textContent = salary.value;
-});
-const name = document.querySelector("#name");
-const nameError = document.querySelector("#name-error");
-name.addEventListener("input", function () {
-  let namRegex = new RegExp(/^[A-Z][a-z]{2,}$/);
-  if (namRegex.test(name.value)) nameError.textContent = "";
-  else nameError.textContent = "Name is Incorrect";
+  salary.addEventListener("input", function () {
+    output.textContent = salary.value;
+  });
 });
 function save() {
   try {
