07c20b6a593b42e13b87b5bdb68cae5754e1652f
vivek <vivek.iitd93@gmail.com>
2021-09-03 20:52:56 +0530

    Create local storage

--- employeeAppjs/js/home.js
+++ employeeAppjs/js/home.js
@@ -1,62 +1,50 @@
+let employeePayrollList;
+
 window.addEventListener("DOMContentLoaded", (event) => {
+  employeePayrollList = getEmployeeFromStorage();
+  console.log(employeePayrollList);
   createInnerHtml();
 });
+
+const getEmployeeFromStorage = () => {
+  return localStorage.getItem("EmployeeList")
+    ? JSON.parse(localStorage.getItem("EmployeeList"))
+    : [];
+};
 
 const createInnerHtml = () => {
   const headerHtml =
     "<tr><th></th><th>Name</th><th>Gender</th><th>Department</th><th>Salary</th><th>Start Date</th><th>Actions</th></tr>";
+  if (employeePayrollList.length == 0) return;
   let innerHtml = `${headerHtml}`;
-  let employeePayrollList = createEmployeePayrollJSON();
   for (const employee of employeePayrollList) {
     innerHtml = `${innerHtml}
-    <tr>
-    <td>
-        <img class="profile" alt="" src="${employee._picture}">
-    </td>
-    <td>${employee._name}</td>
-    <td>${employee._gender}</td>
-    <td>${getDeptHtml(employee._department)}</td>
-    <td>${employee._salary}</td>
-    <td>${employee._startDate}</td>
-    <td>
-        <img id="${
-          employee._id
-        }" onclick="remove(this)" alt="delete" src="../employeeAppjs/assests/icons/delete-black-18dp.svg">
-        <img id="${
-          employee._id
-        }" alt="edit" onclick="update(this)" src="../employeeAppjs/assests/icons/create-black-18dp.svg">
-    </td>
- </tr>
-    `;
+   <tr>
+   <td>
+       <img class="profile" alt="" src="${employee._picture}">
+   </td>
+   <td>${employee._name}</td>
+   <td>${employee._gender}</td>
+   <td>${getDeptHtml(employee._department)}</td>
+   <td>${employee._salary}</td>
+   <td>${employee._startDate}</td>
+   <td>
+       <img name="${
+         employee._id
+       }" onclick="remove(this)" alt="delete" src="../employeeAppjs/assests/icons/delete-black-18dp.svg">
+       <img name="${
+         employee._id
+       }" alt="edit" onclick="update(this)" src="../employeeAppjs/assests/icons/create-black-18dp.svg">
+   </td>
+</tr>
+   `;
   }
   document.querySelector("#display").innerHTML = innerHtml;
 };
 
-const createEmployeePayrollJSON = () => {
-  let employeeListLocal = [
-    {
-      _name: "Vivek",
-      _gender: "Male",
-      _department: ["Engineering"],
-      _salary: "50000",
-      _startDate: "16 Aug 2021",
-      _note: "",
-      _id: new Date().getTime(),
-      _picture: "../employeeAppjs/assests/profile-images/Ellipse -3.png",
-    },
-    {
-      _name: "Laiba",
-      _gender: "Male",
-      _department: ["finance", "Sales"],
-      _salary: "4000",
-      _startDate: "18 Nov 2019",
-      _note: "",
-      _id: new Date().getTime() + 1,
-      _picture: "../employeeAppjs/assests/profile-images/Ellipse -1.png",
-    },
-  ];
-  return employeeListLocal;
-};
+function remove(employeePayrollList) {
+  localStorage.removeItem(employeePayrollList);
+}
 
 const getDeptHtml = (deptList) => {
   let deptHtml = "";
---
+++ employeeAppjs/js/site-properties.js
@@ -0,0 +1,4 @@
+let site_properties = {
+  home_page: "../employeeAppjs/homepage.html",
+  add_emp_payroll_page: "../employeeAppjs/index.html",
+};
