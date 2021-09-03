053ad8b18a501048952197e1d2d8c11080c361ee
vivek <vivek.iitd93@gmail.com>
2021-09-03 21:11:23 +0530

    update Emp details

--- employeeAppjs/js/home.js
+++ employeeAppjs/js/home.js
@@ -12,12 +12,21 @@ const getEmployeeFromStorage = () => {
     : [];
 };
 
+const stringifyDate = (date) => {
+  const options = { day: "numeric", month: "short", year: "numeric" };
+  const newDate = !date
+    ? "undefined"
+    : new Date(Date.parse(date)).toLocaleDateString("en-GB", options);
+  return newDate;
+};
+
 const createInnerHtml = () => {
   const headerHtml =
     "<tr><th></th><th>Name</th><th>Gender</th><th>Department</th><th>Salary</th><th>Start Date</th><th>Actions</th></tr>";
   if (employeePayrollList.length == 0) return;
   let innerHtml = `${headerHtml}`;
   for (const employee of employeePayrollList) {
+    let displayDate = stringifyDate(employee._startDate);
     innerHtml = `${innerHtml}
    <tr>
    <td>
@@ -26,13 +35,13 @@ const createInnerHtml = () => {
    <td>${employee._name}</td>
    <td>${employee._gender}</td>
    <td>${getDeptHtml(employee._department)}</td>
-   <td>${employee._salary}</td>
-   <td>${employee._startDate}</td>
+   <td>&#8377; ${employee._salary}</td>
+   <td>${displayDate}</td>
    <td>
-       <img name="${
-         employee._id
+       <img id="${
+         employee._name
        }" onclick="remove(this)" alt="delete" src="../employeeAppjs/assests/icons/delete-black-18dp.svg">
-       <img name="${
+       <img id="${
          employee._id
        }" alt="edit" onclick="update(this)" src="../employeeAppjs/assests/icons/create-black-18dp.svg">
    </td>
@@ -42,14 +51,30 @@ const createInnerHtml = () => {
   document.querySelector("#display").innerHTML = innerHtml;
 };
 
-function remove(employeePayrollList) {
-  localStorage.removeItem(employeePayrollList);
-}
-
 const getDeptHtml = (deptList) => {
   let deptHtml = "";
   for (const dept of deptList) {
-    deptHtml = `${deptHtml}<div class="dept-label">${dept}</div>`;
+    deptHtml = ` ${deptHtml} <div class="dept-label">${dept}</div>`;
   }
   return deptHtml;
+};
+
+const remove = (node) => {
+  let employeeData = employeePayrollList.find(
+    (empData) => empData._name == node.id
+  );
+  if (!employeeData) return;
+  const index = employeePayrollList
+    .map((empData) => empData._name)
+    .indexOf(employeeData._name);
+  employeePayrollList.splice(index, 1);
+  localStorage.setItem("EmployeeList", JSON.stringify(employeePayrollList));
+  createInnerHtml();
+};
+
+const update = (node) => {
+  let employee = employeePayrollList.find((emp) => emp._name == node.id);
+  if (!employee) return;
+  localStorage.setItem("editEmp", JSON.stringify(employee));
+  window.location.replace(site_properties.add_emp_payroll_page);
 };
